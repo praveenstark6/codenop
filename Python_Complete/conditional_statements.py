49e84069d68571b35c99a7102da94f27354e7f51
praveenkumar <praveentony15@gmail.com>
2022-01-19 10:57:33 +0530

    Create conditional_statements.py

---
+++ Python_Complete/conditional_statements.py
@@ -0,0 +1,37 @@
+# n = 2
+# if n > 2:       
+#     print("greater than 2")
+# elif n == 2:
+#     print("equal to 2")
+# else:
+#     print("lesser than 2")
+
+# output = "greater than 2" if n >= 2 else "lesser than 2"
+
+# print(output)
+
+# false_values = [0, None, [], (), {}, "", False]
+
+# print(bool(0))
+# if 45:
+#     print("greater than 0")
+# else:
+#     print("zero")
+
+
+# print(bool(None))
+# print(bool([]))
+# print(bool(()))
+# print(bool({}))
+# print(bool(""))
+# print(bool(False))
+
+n = 5
+if n >= 0 or n < 10:
+    print("it is a single positive digit")
+else:
+    print("negative or not single digit")
+    
+    
+    
+    
